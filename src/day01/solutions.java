6c4552f8785bbcf9d9404ab8db441ef7011976c3
piotrszacilowski <szacilowski.piotr@gmail.com>
2019-12-01 19:11:50 +0100

    I've added a method to count the required fuel.

--- src/day01/solutions.java
+++ src/day01/solutions.java
@@ -1,4 +1,16 @@
 package day01;
 
 public class solutions {
+
+    public double requiredFuel(double m) {
+        return (Math.floor(m / 3)) - 2;
+    }
+
+    public static void main(String[] args) {
+        solutions solutions = new solutions();
+        System.out.println(solutions.requiredFuel(12));
+        System.out.println(solutions.requiredFuel(14));
+        System.out.println(solutions.requiredFuel(1969));
+        System.out.println(solutions.requiredFuel(100756));
+    }
 }
