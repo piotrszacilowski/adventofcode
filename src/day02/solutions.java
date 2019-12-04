cf583f45800141d6b4b913aac107421ae5c7e279
Piotr Szaciłowski <szacilowski.piotr@gmail.com>
2019-12-04 01:23:23 +0100

    Added 2nd day.

---
+++ src/day02/solutions.java
@@ -0,0 +1,11 @@
+package day02;
+
+import java.io.File;
+
+public class solutions {
+    public static void main(String[] args) {
+        String nameFile = "input/input02.txt";
+        File file = new File(nameFile);
+        
+    }
+}
