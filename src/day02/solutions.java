bb1455363a8474adc1939c79b71562dbab468f4c
piotrszacilowski <szacilowski.piotr@gmail.com>
2019-12-06 00:18:34 +0100

    Solution for 2.1. Optcode conditions.

--- src/day02/solutions.java
+++ src/day02/solutions.java
@@ -1,11 +1,26 @@
 package day02;
 
 import java.io.File;
+import java.util.List;
 
 public class solutions {
     public static void main(String[] args) {
         String nameFile = "input/input02.txt";
         File file = new File(nameFile);
-        
+
+
+        g[1] = 12;
+        g[2] = 2;
     }
-}
+
+    private int counter(List<String> intCode) {
+        while (g[value] != 99) {
+            if (g[value] == 1) {
+
+            } else if (g[value] == 2) {
+
+            } else {
+
+            }
+        }
+    }
